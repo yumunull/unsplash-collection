679a63a8c9f9295ebf7704c08ba5bb1482498404
MSI\Leo <leo.hsu@yudutek.cc>
2025-06-05 17:58:18 +0800

    working on collection preview photos

--- app/_components/CollectionsPreview.tsx
+++ app/_components/CollectionsPreview.tsx
@@ -1,15 +1,31 @@
+import Image from "next/image";
 import {FC} from "react";
 import {useCollections} from "@/stores/useCollections";
+
 interface Props {
     index: number,
 }
+
 const CollectionsPreview: FC<Props> = ({index}) => {
     const {collections} = useCollections()
     if (index >= collections.length) return <></>
     const collection = collections[index]
+    const getPreviewPhoto = (index: number) => {
+        return collection?.preview_photos?.at(index) ?
+            <Image src={collection.preview_photos[index].urls.raw} alt={"preview photo"} fill/> : <></>
+    }
     return (
-        <div>
-            <span>{collection.title}</span>
+        <div className={"w-full h-full flex flex-col"}>
+            <div className={"grid grid-rows-2 grid-cols-[7fr_3fr] grow gap-1 [&_*]:rounded-sm [&_*]:overflow-hidden"}>
+                <div className={"bg-gray relative row-span-2"}>
+                    {
+                        getPreviewPhoto(0)
+                    }
+                </div>
+                <div className={"bg-gray relative"}></div>
+                <div className={"bg-gray relative"}></div>
+            </div>
+            <span className={""}>{collection.title}</span>
             <span>{collection.total_photos} photo{collection.total_photos > 1 && "s"}</span>
         </div>
     )
--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
             protocol: "https",
             hostname: "images.unsplash.com",
             pathname: "/**"
+        },{
+            protocol: "https",
+            hostname: "plus.unsplash.com",
+            pathname: "/**"
         }]
     }
 };
